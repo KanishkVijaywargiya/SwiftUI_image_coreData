32793486547396922bc14d09567200285f036899
KanishkVijaywargiya <secretcodersociety@gmail.com>
2023-01-14 15:58:17 +0530

    Initial Commit

---
+++ CoreDataBootcamp/CoreDataBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataBootcampApp.swift
+//  CoreDataBootcamp
+//
+//  Created by Kanishk Vijaywargiya on 14/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
